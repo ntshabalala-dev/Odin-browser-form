dcc498dea618ad4c8c8f82d75d05dbe0196ebe06
Nhlanhla <ntshabalalait@gmail.com>
2026-08-30 20:09:05 +0200

    feat: implement form validation and error handling for registration fields

--- src/script.js
+++ src/script.js
@@ -1,25 +1,101 @@
 import "../src/main.css";
 
 const log = console.log;
-const email = document.querySelector('#email')
-const form = document.querySelector('form');
+// Inputs
+const email = document.querySelector("#email");
+const country = document.querySelector("#country");
+const postalCode = document.querySelector("#postalCode");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirmPassword");
 
-log(email.validity.valid)
+const inputs = document.querySelectorAll(".form-group input");
 
+inputs.forEach((input) => {
+    const errorName = input.id;
+    const error = document.querySelector(`.form-group.${errorName} .error`);
+
+    input.addEventListener("input", () => {
+        if (input.value.trim().length === 0) {
+            input.required = false;
+        }
+
+        if (input.validity.valid) {
+            error.textContent = ""; // Remove the message content
+            error.className = "error"; // Removes the 
+        } else {
+
+            const s = errorName.charAt(0).toUpperCase() + errorName.slice(1);
+            const functionName = `show${s}Error`;
+            actions[functionName](input, error);
+        }
+    });
+});
+
+const errorSpans = document.querySelectorAll(".form-group input ~ .error");
+
+log(errorSpans);
+
+const form = document.querySelector("form");
+
+// Error messages
+
+// Submit button
 form.addEventListener("submit", (event) => {
+
+    inputs.forEach((input) => {
+        if (input.value.trim().length === 0) {
+            input.required = true;
+        }
+    })
     // if the email field is invalid
+    event.preventDefault();
+    log(email.validity.valid);
     if (!email.validity.valid) {
         // display an appropriate error message
-        //showError();
+        log("hello");
+        const emailError = document.querySelector(".form-group.email .error");
+
+        showEmailError(email, emailError);
         // prevent form submission
-        event.preventDefault();
+    }
+
+    if (!country.validity.valid) {
+        // display an appropriate error message
+        const CountryErrorSpan = document.querySelector(
+            `.form-group.country .error`,
+        );
+        showDropDownError(country, CountryErrorSpan);
+        // prevent form submission
     }
 });
 
-email.addEventListener('input', () => {
-    if (email.validity.valid) {
-        log('email is valid');
-    } else {
-        log('email is not valid');
+function showEmailError(input, errorMessageSpan) {
+    if (input.validity.valueMissing) {
+        // If empty
+        errorMessageSpan.textContent = "You need to enter an email address.";
+    } else if (input.validity.typeMismatch) {
+        // If it's not an email address,
+        errorMessageSpan.textContent =
+            "Entered value needs to be an email address.";
+    } else if (input.validity.tooShort) {
+        // If the value is too short,
+        errorMessageSpan.textContent = `Email should be at least ${email.minLength} characters; you entered ${input.value.length}.`;
     }
-});
+
+    log(errorMessageSpan.textContent);
+    // Add the `active` class
+    errorMessageSpan.className = "error active";
+}
+
+function showDropDownError(input, errorMessageSpan) {
+    // log(country.validity)
+    if (input.validity.valueMissing) {
+        errorMessageSpan.textContent = "Please select an option from the list.";
+    }
+
+    errorMessageSpan.className = "error active";
+}
+
+const actions = {
+    showEmailError: showEmailError,
+};
