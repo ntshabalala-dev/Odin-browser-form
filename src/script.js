3209bd8c9d846674d4c75719ff6660a2a6da03cf
Nhlanhla <ntshabalalait@gmail.com>
2026-08-30 14:00:46 +0200

    feat: add validation error messages and styles to registration form

--- src/script.js
+++ src/script.js
@@ -1,1 +1,16 @@
 import "../src/main.css";
+
+const log = console.log;
+const email = document.querySelector('#email')
+
+log(email.validity.valid)
+
+form.addEventListener("submit", (event) => {
+    // if the email field is invalid
+    if (!email.validity.valid) {
+        // display an appropriate error message
+        //showError();
+        // prevent form submission
+        event.preventDefault();
+    }
+});
